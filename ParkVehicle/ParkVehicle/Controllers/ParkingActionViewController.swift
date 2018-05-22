f4e269cc64ab002b374a7c22f633c45a515b13d9
Prajakta Kulkarni <prachee.j@gmnail.com>
2018-05-22 22:10:36 +0200

    added code for creating parking action via api, patch parking action.

--- ParkVehicle/ParkVehicle/Controllers/ParkingActionViewController.swift
+++ ParkVehicle/ParkVehicle/Controllers/ParkingActionViewController.swift
@@ -13,13 +13,28 @@ class ParkingActionViewController: UIViewController,UITableViewDataSource,UITabl
     var parkingActionDetails:NSMutableDictionary!
     var keys:NSArray!
     var values:NSArray!
+    var apiManager = APIManager()
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         keys = parkingActionDetails.allKeys as NSArray
         values = parkingActionDetails.allValues as NSArray
+        
+        //apiManager.submit
         // Do any additional setup after loading the view.
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        var myDict: NSDictionary?
+        if let path = Bundle.main.path(forResource: "API", ofType: "plist") {
+            myDict = NSDictionary(contentsOfFile: path)
+        }
+        apiManager.post(data: parkingActionDetails, path: myDict?.value(forKey: "CreateParkingAction") as! String) { (error,string) in
+      
+                print(error ?? "Posted successfully")
+            self.parkingActionDetails.setValue(string, forKey: "parkingActionId")
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -39,14 +54,26 @@ class ParkingActionViewController: UIViewController,UITableViewDataSource,UITabl
         return cell
     }
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        var myDict: NSDictionary?
+        if let path = Bundle.main.path(forResource: "API", ofType: "plist") {
+            myDict = NSDictionary(contentsOfFile: path)
+        }
+        var path = myDict?.value(forKey: "PatchParkingAction") as! String
+        path = path.replacingOccurrences(of: "userId", with: parkingActionDetails.value(forKey: "parkingActionId") as! String)
+        apiManager.patch(data: parkingActionDetails, path: path) { (error,string) in
+            
+            print(error ?? "Posted successfully")
+            //self.parkingActionDetails.setValue(string, forKey: "parkingActionId")
+        }
+
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
+ 
 
 }
--- ParkVehicle/ParkVehicle/Utility/APIManager.swift
+++ ParkVehicle/ParkVehicle/Utility/APIManager.swift
@@ -16,7 +16,7 @@ class APIManager{
     
    
     
-    func getPosts(for userId: Int, path:String, completion: ((Result<Data>) -> Void)?) {
+    func GetData(form path:String, completion: ((Result<Data>) -> Void)?) {
         var urlComponents = URLComponents()
         var myDict: NSDictionary?
         if let path = Bundle.main.path(forResource: "API", ofType: "plist") {
@@ -70,49 +70,110 @@ class APIManager{
         
     }
     
-    func submitPost(post: User, completion:((Error?) -> Void)?) {
-        var urlComponents = URLComponents()
-        urlComponents.scheme = "https"
-        urlComponents.host = "jsonplaceholder.typicode.com"
-        urlComponents.path = "/posts"
-        guard let url = urlComponents.url else { fatalError("Could not create URL from components") }
-        
-        // Specify this request as being a POST method
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        // Make sure that we include headers specifying that our request's HTTP body
-        // will be JSON encoded
-        var headers = request.allHTTPHeaderFields ?? [:]
-        headers["Content-Type"] = "application/json"
-        request.allHTTPHeaderFields = headers
-        
-        // Now let's encode out Post struct into JSON data...
-        let encoder = JSONEncoder()
-        do {
-            let jsonData = try encoder.encode(post)
-            // ... and set our request's HTTP body
-            request.httpBody = jsonData
-            print("jsonData: ", String(data: request.httpBody!, encoding: .utf8) ?? "no body data")
-        } catch {
-            completion?(error)
+    func post(data: NSDictionary , path:String , completion:((Error?,String) -> Void)?) {
+        var myDict: NSDictionary?
+        if let path = Bundle.main.path(forResource: "API", ofType: "plist") {
+            myDict = NSDictionary(contentsOfFile: path)
         }
-        
-        // Create and run a URLSession data task with our JSON encoded POST request
-        let config = URLSessionConfiguration.default
-        let session = URLSession(configuration: config)
-        let task = session.dataTask(with: request) { (responseData, response, responseError) in
-            guard responseError == nil else {
-                completion?(responseError!)
-                return
+        var urlComponents = URLComponents()
+        if let dict = myDict{
+            urlComponents.scheme = dict.value(forKey: "scheme") as? String
+            urlComponents.host = dict.value(forKey: "host") as? String
+            urlComponents.path = path
+            guard let url = urlComponents.url else { fatalError("Could not create URL from components") }
+            
+            // Specify this request as being a POST method
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST"
+            // Make sure that we include headers specifying that our request's HTTP body
+            // will be JSON encoded
+            var headers = request.allHTTPHeaderFields ?? [:]
+            headers["Content-Type"] = "application/json"
+            request.allHTTPHeaderFields = headers
+            
+            // Now let's encode out Post struct into JSON data...
+            do {
+                let jsonData = try JSONSerialization.data(withJSONObject: data, options: JSONSerialization.WritingOptions.sortedKeys)
+                // ... and set our request's HTTP body
+                request.httpBody = jsonData
+                print("jsonData: ", String(data: request.httpBody!, encoding: .utf8) ?? "no body data")
+            } catch {
+                completion?(error,"")
             }
             
-            // APIs usually respond with the data you just sent in your POST request
-            if let data = responseData, let utf8Representation = String(data: data, encoding: .utf8) {
-                print("response: ", utf8Representation)
-            } else {
-                print("no readable data received in response")
+            // Create and run a URLSession data task with our JSON encoded POST request
+            let config = URLSessionConfiguration.default
+            let session = URLSession(configuration: config)
+            let task = session.dataTask(with: request) { (responseData, response, responseError) in
+                guard responseError == nil else {
+                    completion?(responseError!,"")
+                    return
+                }
+                
+                // APIs usually respond with the data you just sent in your POST request
+                if let data = responseData, let utf8Representation = String(data: data, encoding: .utf8) {
+                    print("response: ", utf8Representation)
+                    completion?(nil,utf8Representation)
+                } else {
+                    print("no readable data received in response")
+                }
+
             }
+            task.resume()
         }
-        task.resume()
+        
+    }
+    func patch(data: NSDictionary , path:String , completion:((Error?,String) -> Void)?) {
+        var myDict: NSDictionary?
+        if let path = Bundle.main.path(forResource: "API", ofType: "plist") {
+            myDict = NSDictionary(contentsOfFile: path)
+        }
+        var urlComponents = URLComponents()
+        if let dict = myDict{
+            urlComponents.scheme = dict.value(forKey: "scheme") as? String
+            urlComponents.host = dict.value(forKey: "host") as? String
+            urlComponents.path = path
+            guard let url = urlComponents.url else { fatalError("Could not create URL from components") }
+            
+            // Specify this request as being a POST method
+            var request = URLRequest(url: url)
+            request.httpMethod = "PATCH"
+            // Make sure that we include headers specifying that our request's HTTP body
+            // will be JSON encoded
+            var headers = request.allHTTPHeaderFields ?? [:]
+            headers["Content-Type"] = "application/json"
+            request.allHTTPHeaderFields = headers
+            
+            // Now let's encode out Post struct into JSON data...
+            do {
+                let jsonData = try JSONSerialization.data(withJSONObject: data, options: JSONSerialization.WritingOptions.sortedKeys)
+                // ... and set our request's HTTP body
+                request.httpBody = jsonData
+                print("jsonData: ", String(data: request.httpBody!, encoding: .utf8) ?? "no body data")
+            } catch {
+                completion?(error,"")
+            }
+            
+            // Create and run a URLSession data task with our JSON encoded POST request
+            let config = URLSessionConfiguration.default
+            let session = URLSession(configuration: config)
+            let task = session.dataTask(with: request) { (responseData, response, responseError) in
+                guard responseError == nil else {
+                    completion?(responseError!,"")
+                    return
+                }
+                
+                // APIs usually respond with the data you just sent in your POST request
+                if let data = responseData, let utf8Representation = String(data: data, encoding: .utf8) {
+                    print("response: ", utf8Representation)
+                    completion?(nil,utf8Representation)
+                } else {
+                    print("no readable data received in response")
+                }
+                
+            }
+            task.resume()
+        }
+        
     }
 }
